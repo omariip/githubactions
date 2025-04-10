6525d88fbb9a4a518d5a2bacfc7dca5070a43ee3
Omar Abou Chaer <aboushaar.omar@gmail.com>
2025-04-10 09:12:38 -0400

    Trigger my name

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req, res) => {
- res.send('Hello, Dockerized Node.js App!');
+app.get("/", (req, res) => {
+  res.send("Hello, Im Omar :)!");
 });
 app.listen(PORT, () => {
- console.log(`App running on http://localhost:${PORT}`);
+  console.log(`App running on http://localhost:${PORT}`);
 });
